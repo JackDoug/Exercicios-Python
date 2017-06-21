9da0ccb02ab73773fb3da5e5d860374930d69382
JackDoug <jackson_cvieira@outlook.com>
2017-06-21 20:54:27 -0300

    Update cigarro.py

--- cigarro.py
+++ cigarro.py
@@ -1,5 +1,10 @@
+##quantidade de min que perderá a cada cigarro##
 c=int(input("digite quantos cigarros voce fuma por dia? "))
-print (c)
-a=int(input("digiter quantos anos voce fuma?"))
-print (a)
+a=int(input("digite quantos anos voce fuma?"))
 
+cd=(c*365)*a
+
+
+mp=(cd*10)
+hp=(mp/60)
+print("quantidade de dias de vida que perderá ", hp  )
