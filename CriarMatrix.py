b2a617d2c125f535c7a16abc95bda3309d777e8d
JackDoug <jackson_cvieira@outlook.com>
2017-06-21 19:49:24 -0300

    Update CriarMatrix.py

--- CriarMatrix.py
+++ CriarMatrix.py
@@ -1,20 +1,4 @@
-###vetor
-# from random import randint
-# l=["fabiano","joão","maria"]
-# s=["1","2","3"]
-# x=randint(0,2)
-# cont=0
-# print(x)
-# print(l[x])
-# while cont<3:
-#    resp=input("Digite a senha:")
-#    if resp==s[x]:
- #       print("Acesso Liberato")
- #    break
- #   cont=cont+1
-##################
-
-##Matriz:###
+###Criando Vetor para armazenar valores##
 
 from random import randint
 l=[["fabiano","joão","maria"],["1","2","3"]]
