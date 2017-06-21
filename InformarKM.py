0b664411c365caa8c171170004597948546ed04d
JackDoug <jackson_cvieira@outlook.com>
2017-06-21 20:01:01 -0300

    Update InformarKM.py

--- InformarKM.py
+++ InformarKM.py
@@ -1,3 +1,5 @@
+##calculo do consumo do carro##
+
 km=int(input("Informar a km com tanque cheio "))
 p=int(input("informar kms percorridos "))
 c=int(input("combustível necessário para completar o tanque "))
