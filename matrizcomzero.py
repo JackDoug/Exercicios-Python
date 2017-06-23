2c2266372d788fdb2bd6a960cb8ad72a8a77dd5d
JackDoug <jackson_cvieira@outlook.com>
2017-06-23 18:28:05 -0300

    Create matrizcomzero.py

---
+++ matrizcomzero.py
@@ -0,0 +1,11 @@
+linha=int(input("linhas: "))
+colunas=int(input("colunas: "))
+m = [0]*linha
+#ele ta repetindo o 0 a quantidade de vezes de linha q o usuario digitou
+
+
+
+
+for i in range(linha):
+    m[i] = [0]*colunas #ele ta repetindo o 0 a quantidade de vezes de colunas q o usuário digitou
+print(m)
